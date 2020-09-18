462b24f6ecc1bb0971cad1c2f60c28e91e16a813
Ryan Whitmore <ryan@whitmorespace.com>
2020-09-17 19:25:32 -0700

    switching servers.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT:
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
+  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL,
   TOKEN_KEY: 'chargen-client-auth-token'
 };
